191905764dba64b44a64d35facd550c6bbe7f988
Ferry Utomo <huferry@gmail.com>
2025-07-22 13:33:55 +0200

    Adding responsive voice key

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const fs = require('fs').promises
 const {join} = require('path')
+const path = require('path')
+const dotenv = require('dotenv')
+
+dotenv.config()
 
 const app = express();
 const port = process.env.PORT || 3000; // Use PORT from environment or default to 3000
@@ -15,8 +19,35 @@ async function getDictionaries() {
 }
 
 const dictionaries = []
-  
-app.use('/', express.static('public'));
+
+app.use(async (req, res, next) => {
+  const filePath = getFilePath(req)
+  try {
+    await fs.access(filePath);
+    if (filePath.endsWith('.html')) {
+      const content = await fs.readFile(filePath, 'utf-8');
+      res.setHeader('Content-Type', 'text/html');
+      res.send(processHtmlContent(content));
+    } else {
+      res.sendFile(filePath);
+    }
+  } catch (err) {
+    next();
+  }
+});
+
+function getFilePath(req) {
+  return req.path === '/' ? path.join(__dirname, 'public', 'index.html') : path.join(__dirname, 'public', req.path);
+}
+
+function processHtmlContent(html) {
+  const match = html.match(/\{{2}ENV:([A-Z|_]+)\}{2}/)
+  if (match) {
+    const value = process.env[match[1]]
+    return html.replace(match[0], value)
+  } 
+  return html
+}
 
 app.get('/api/dict/index', (req, res) => {
   res.json(dictionaries.map(d => {
